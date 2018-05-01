fb4bf060a8849b8a5e7f6ffe7a1ebd66611b7525
yuchiu <yuchiu2002@hotmail.com>
2018-05-01 17:47:01 -0400

    update

--- moodlebuild.sh
+++ moodlebuild.sh
@@ -1,24 +1,25 @@
+
 #!/bin/bash
 
 MOODLEVERSION=''
-MYSQLPASSWORD='root'
 
 #install NGINX
 
-yes Y | sudo apt-get install nginx
+sudo apt-get -y install nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
 #install & setup MySQL
-#sh /home/yuchiu/Desktop/removemysql.sh
-sudo debconf-set-selections <<< 'mysql-server root'
-sudo debconf-set-selections <<< 'mysql-server root'  
 sudo apt-get -y install mysql-server mysql-client
+
+#sudo apt-get -y install mysql-server mysql-client
+
 sudo systemctl start mysql
 sudo systemctl enable mysql
 ####sudo sed -i 's/Basic Settings/Basic Settings\ndefault_storage_engine = innodb\ninnodb_file_per_table = 1\ninnodb_file_format = Barracuda/' /etc/mysql/mysql.conf.d/mysqld.cnf
 
 #Create database and user for Moodle
 
-##sudo mysql -u root -p
-##CREATE DATABASE moodledb;
+sudo mysql -u root -proot -Bse "CREATE DATABASE moodledb;ALTER DATABASE moodledb DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;FLUSH PRIVILEGES;CREATE USER moodleuser@localhost IDENTIFIED BY moodlepassword;GRANT ALL ON moodledb.* TO moodleuser@localhost;FLUSH PRIVILEGES;EXIT;"
+
+sudo apt-get -y install php7.0-fpm graphviz aspell php7.0-pspell php7.0-curl php7.0-gd php7.0-intl php7.0-mysql php7.0-xml php7.0-xmlrpc php7.0-ldap php7.0-zip php7.0-soap php7.0-mbstring
